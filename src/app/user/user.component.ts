9890633a36decd8659bf98a2c3fef1685ef045f7
jerumanu <emmanueljeru200@gmail.com>
2020-11-16 13:46:51 +0300

    add function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+import{ User} from '../user';
+import {Repo} from '../repo'
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-user',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+ 
+  user:User;
+  userrepos:Repo
+  User = environment.apiKey;
 
-  constructor() { }
+  constructor(public profileService:ProfileService ) { }
+
+  getUser(UserName){
+    this.profileService.getUser(UserName).then(
+     ( success)=>{
+       this.user = this.profileService.user;
+     },
+     (error)=>{
+       
+     }
+    )
+
+
+    this.profileService.getUserRepos(UserName).then(
+      ( success)=>{
+        this.userrepos = this.profileService.userrepos;
+      
+        
+        
+
+
+        
+      },
+      (error)=>{
+        console.log(error)
+      }
+     )
+  }
+
 
   ngOnInit(): void {
+    this.getUser("jerumanu");
+    
   }
 
 }
